a6bb78329c11a75367c08e1a8c3c21d58fc243b1
Bozhidar Batsov <bozhidar@batsov.dev>
2021-08-17 11:24:58 +0300

    Add deployment metadata

--- project.clj
+++ project.clj
@@ -4,4 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
+
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password
+                                    :sign-releases false}]]
+
   :repl-options {:init-ns incomplete.core})
