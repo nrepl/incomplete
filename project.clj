562398bf8da703da094dd54d9c9f6d85b58a123f
Bozhidar Batsov <bozhidar@batsov.dev>
2021-08-26 13:58:10 +0300

    Update the clojure dep

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/nrepl/incomplete"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  ;; we actually target Clojure 1.7, but some dev tools no longer support it
+  ;; TODO: Add several profiles for various Clojure versions here.
+  :dependencies [[org.clojure/clojure "1.8.0"]]
 
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_username
